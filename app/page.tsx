b7d8e648e3fefb2be92e879d220caeaffc2b5994
daichi-iwamoto <tay1fai0y@gmail.com>
2024-02-22 23:29:22 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
           image={image}
           channels={channels}
           userName={name}
-          channelId={channelId}
+          channelId={channelId || ""}
         />
         {ranking && channelId ? <Ranking ranking={ranking} /> : null}
       </main>
